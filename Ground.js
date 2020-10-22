057293c464dd4b544e81a4c70341167d76cf1860
adalal45 <72637974+adalal45@users.noreply.github.com>
2020-10-21 19:04:39 -0700

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,16 @@
+class Ground {
+  constructor(x, y, width, height) {
+    var Options = {
+      isStatic: true,
+    };
+    this.body = Bodies.rectangle(x, y, width, height, Options);
+    this.width = width;
+    this.height = height;
+    World.add(world, this.body);
+  }
+  display() {
+    rectMode(CENTER);
+    var pos = this.body.position;
+    rect(pos.x, pos.y, this.width, this.height);
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,27 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine;
+var world;
+var object;
+var box1, box2;
+
+function setup() {
+  createCanvas(400, 400);
+  engine = Engine.create();
+  world = engine.world;
+  box1 = new Box(200, 200, 50, 50);
+  box2 = new Box(240, 100, 50, 100);
+  ground = new Ground(200, 300, 400, 10);
+}
+
+function draw() {
+  background(0);
+  Engine.update(engine);
+
+  // rect(200, 200, 50, 50);
+  box1.display();
+  box2.display();
+  ground.display();
+}
